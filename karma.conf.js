029dbaee37935d44de9a9b8ffd2bb6e615e9cdbc
Mohammed Shoaib <mdshoaib093@gmail.com>
2021-10-04 08:23:50 +0530

    Initial commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/taskList'),
+      dir: require('path').join(__dirname, './coverage/tasklist'),
       subdir: '.',
       reporters: [
         { type: 'html' },
